fc5ad379253cf73ede60ada9754777c54a52064b
Lyndon Shi <shilyndon@gmail.com>
2020-11-29 22:40:37 -0800

    some model changes

--- league_history_collector/analyzer/models/season.py
+++ league_history_collector/analyzer/models/season.py
@@ -3,19 +3,18 @@
 from dataclasses import dataclass
 from typing import Dict, List
 
-from league_history_collector.models import Game
+from league_history_collector.models import Record
 from league_history_collector.utils import CamelCasedDataclass
 
 
 @dataclass
-class Season(CamelCasedDataclass):
+class Season(CamelCasedDataclass):  # pylint: disable=too-many-instance-attributes
     """Stores statistics about a season."""
 
-    champion: str
     final_standings: Dict[str, int]
-    playoff_games: Dict[int, Game]
 
     playoff_teams = List[str]
     regular_season_standings: Dict[str, int]
+    regular_season_records: Dict[str, Record]
     regular_season_points_for: Dict[str, float]
-    regular_season_points_again: Dict[str, float]
+    regular_season_points_against: Dict[str, float]
